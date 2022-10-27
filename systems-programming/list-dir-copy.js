67f582fe57aecd5afa5a6bc9d51d43fba5221d4b
Tim Mastny <tim.mastny@gmail.com>
2022-10-26 19:24:37 -0500

    save

--- systems-programming/list-dir-copy.js
+++ systems-programming/list-dir-copy.js
@@ -1,12 +1,17 @@
 import glob from 'glob'
 import path from 'path'
+import fs from 'fs-extra'
 
 const [srcDir, dstDir] = process.argv.slice(2)
 
 console.log(path.dirname(dstDir))
 glob(`${srcDir}/*.*`, (err, files) => {
     for (const file of files) {
-        const new_file = file.replace(srcDir, dstDir)
-        console.log(file, new_file)
+        fs.stat(file, (err, stats) => {
+            if (stats.isFile()) {
+                const new_file = file.replace(srcDir, dstDir)
+                console.log(file, new_file)
+            }
+        })
     }
 })
